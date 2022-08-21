94e48770e22b738574a1c6bbf91726307dd58403
KonstChurakaev <111491525+KonstChurakaev@users.noreply.github.com>
2022-08-21 23:22:43 +0500

    Update Check.java

--- src/main/java/Check.java
+++ src/main/java/Check.java
@@ -1,44 +1,25 @@
-
-public class Check {
-    public static String validatingInput(String input) {
+class Check {
+    public static String validatingInput(String numberOne, String numberTwo, String operation) {
 
         String arabicNumerals = "12345678910";
         String romanNumerals = "IIIVIIIX";
-        String operation = "+-*/";
 
-        input = input.toUpperCase();
-        String[] temp = input.split(" ");
-
-        if (temp.length > 3) {
-            return "throws Exception// the format of the mathematical operation does not satisfy the task - two operands and one operator (+, -, /, *)";
-        }
-        if (temp.length<3){
-            return "throws Exception// string is not a mathematical operation";
-        }
-        if (!operation.contains(temp[1])) {
-            return "Incorrectly entered operation";
-        }
-        if (temp[1].equals("/")&&temp[2].equals("0")){
-            return "ArithmeticException// division by zero";
-        }
-
-        if (arabicNumerals.contains(temp[0])) {
-            if (arabicNumerals.contains(temp[2])){
-                return String.valueOf(Arabic.changeArabikNumbA(input));
+        if (arabicNumerals.contains(numberOne)) {
+            if (arabicNumerals.contains(numberTwo)) {
+                return Arabic.changeArabikNumbA(numberOne, numberTwo, operation);
             }
-            if (romanNumerals.contains(temp[2])){
-                System.out.println("different number systems are used at the same time");
-            }
-
-        }
-        if (romanNumerals.contains(temp[0])) {
-            if (romanNumerals.contains(temp[2])){
-                return Roman.changeRomanNumb(input);
-            }
-            if (arabicNumerals.contains(temp[2])){
+            if (romanNumerals.contains(numberTwo)) {
                 System.out.println("different number systems are used at the same time");
             }
         }
-return "Wrong data entry";
+        if (romanNumerals.contains(numberOne)) {
+            if (romanNumerals.contains(numberTwo)) {
+                return Roman.changeRomanNumb(numberOne, numberTwo, operation);
+            }
+            if (arabicNumerals.contains(numberTwo)) {
+                System.out.println("different number systems are used at the same time");
+            }
+        }
+        return "Wrong data entry";
     }
 }
